da237ecd2438a7ff3c05b394ddc1cd45d2e61246
Hamza <h@local>
2022-01-27 08:06:49 +0300

    Fix: separate the dark mode button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,25 @@ import Cart from "../sections/cart";
 import Details from "../sections/details";
 import Head from "next/head";
 
+let cartBg: any;
+let cartSecondTextColor: any;
+
+export const DarkModeSw = () => {
+  
+  const {toggleColorMode} = useColorMode();
+  cartBg = useColorModeValue('gray.100', 'whiteAlpha.50');
+  cartSecondTextColor = useColorModeValue('gray.600', 'gray.400');
+
+  return (
+    <Stack alignItems="center" justifyContent="space-around" py={1}>
+        <Button onClick={toggleColorMode}>🌓 Mode</Button>
+    </Stack>
+  )
+};
+
 
 
 const indexPage = () => {
-  const {toggleColorMode} = useColorMode();
-  const cartBg = useColorModeValue('gray.100', 'whiteAlpha.50');
-  const cartSecondTextColor = useColorModeValue('gray.600', 'gray.400');
 
   return (
     <>
@@ -17,10 +30,8 @@ const indexPage = () => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Container maxW="container.xl" p={0}>
-      <Stack alignItems="center" justifyContent="space-around" py={1}>
-        <Button onClick={toggleColorMode}>🌓 Mode</Button>
-      </Stack>
-      <Flex h={{base: 'auto', md: '100vh'}} py={[0, 10, 20]} direction={{base: 'column-reverse', md: 'row'}}>
+      <DarkModeSw/>
+      <Flex h={{base: 'auto', md: '100vh'}} py={[0, 0, 0]} direction={{base: 'column-reverse', md: 'row'}}>
         <Details />
         <Cart cartBg={cartBg} cartSecondTextColor={cartSecondTextColor}/>
       </Flex>
